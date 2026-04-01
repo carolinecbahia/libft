fc97f2e89f6df43a2d41c9329885fb0c4058c545
Caroline Bahia <carolinecbahia@gmail.com>
2026-04-01 13:51:04 -0300

    Implement ft_strcmp function for string comparison

---
+++ src/addtional/ft_strcmp.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccavalca <ccavalca@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/30 01:12:11 by ccavalca          #+#    #+#             */
+/*   Updated: 2026/01/30 01:12:59 by ccavalca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t  i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
